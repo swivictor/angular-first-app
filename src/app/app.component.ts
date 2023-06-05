d81724c25d2688b96c454b18196ec7fb51c49ffc
Victor B <victor@semanticwebindia.com>
2023-06-05 12:32:35 +0530

    Part 3 Complted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Default</h1>`,
+  imports: [HomeComponent],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo">
+    </header>
+    <section class="content">
+      <!-- NOTE - Any other name can given other than <app-home> -->
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+   <section>
+    <form>
+      <input type="text" placeholder="filter by city">
+      <button class="primary" type="botton">Search</button>
+    </form>
+   </section>
+   <section class="result">
+    <app-housing-location></app-housing-location>
+   </section>
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
